03a28d01cf8d6efe8b19e89c5ddac90b93f9d9c7
Vendy Vokalkova <vendula.vokalkova@flowup.cz>
2020-05-06 13:08:54 +0200

    add new parts of test

--- po.js
+++ po.js
@@ -1,27 +1,25 @@
-const {By} = require('selenium-webdriver');
-
-class Page {
-    constructor(driver) {
-        this.driver = driver;
-    }
-
-    getZpravicky() {
-        return this.driver.findElement(this.zpravickyLink);
-    }
-
-    getKartaSeZpravickou() {
-        return this.driver.findElement(this.getKartaSeZpravickou()).index();
-    }
-}
-
-class RootPage extends Page {
-    constructor(driver) {
-        super(driver);
-        this.zpravickyLink = By.css('.navigation__link[href="/zpravicky/"]');
-        this.kartaSeZpravickou = By.css('.design-box__content .design-list__item');
-        // this.datumKarty;
-
-    }
-}
-
-module.exports.RootPage = RootPage;
+// class Page {
+//     constructor(browser) {
+//         this.browser = browser;
+//     }
+//
+//     getZpravicky() {
+//         return this.browser.findElement(this.zpravickyLink);
+//     }
+//
+//     getKartaSeZpravickou(number) {
+//         return this.browser.findElement.all(this.getKartaSeZpravickou()).get(number);
+//     }
+// }
+//
+// class RootPage extends Page {
+//     constructor(browser) {
+//         super(browser);
+//         this.zpravickyLink = by.css('.navigation__link[href="/zpravicky/"]');
+//         this.kartaSeZpravickou = by.css('.design-box__content .design-list__item .design-article--without-image.design-article design-tile');
+//         // this.datumKarty;
+//
+//     }
+// }
+//
+// module.exports.RootPage = RootPage;
--- test.js
+++ test.js
@@ -1,20 +1,56 @@
-let page = require('./po');
 require('geckodriver');
 const webdriver = require('selenium-webdriver');
 
+
 describe('Protractor Demo App', function() {
-    let driver;
-    let rootPageObject;
-
     beforeEach(async function() {
-        driver = new webdriver.Builder().forBrowser('firefox').build();
-        rootPageObject = new page.RootPage(driver);
-        await browser.get('http://juliemr.github.io/protractor-demo/');
+        await browser.waitForAngularEnabled(false);
+        await browser.get('https://www.root.cz');
     });
 
-    it('should have a title', function() {
-       console.log('I am in');
+    it('found todays "zprávičky" on the root.com and count them. ', async function () {
+        console.log('I am in!');
+        // await browser.sleep(2000);
+        const zpravickyLink = await element.all(by.css('.design-navigation__layout-cell .navigation__link[href="/zpravicky/"]')).get(0);
+        // const EC = protractor.ExpectedConditions;
+        // await browser.wait(EC.invisibilityOf(zpravickyLink),8000);//todo: find why it doesn't work
+        await zpravickyLink.click();
+        await console.log('Otevře se stránka se Zprávičkami.');
+        await browser.sleep(3000);
+        const kartySeZpravickami = await element.all(by.css('.design-article--without-image.design-article.design-tile')).get(0);
+        // const kartaSeZpravickou = kartySeZpravickami.get(0);
+        const datum = kartySeZpravickami.element(by.css('span.design-impressum__item')).getText();
+        await console.log(datum);
+        await expect(await datum).toEqual('Dnes');
+        await browser.sleep(3000);
+
+        for(let i = 0 ; i<5; i++){
+
+
+        }
+
     });
+
+//  const kartaSeZpravickou = driver.getElement.all(by.css('karta se zpravickou');
+//  const spodniCastKarty = kartaSeZpravickou.element(by.css('spodniCastKartyElement');
+//  const datum = spodniCastKart.element(by.css('datum').getText();
+//
+//  const dnesniZpravicky = [];
+//  const zpravicka;
+//
+//  for (i = 0; i < (notNowKnownNumber); i++) {
+//    kartaSeZpravickou.get(index++)//nevím zatím jak to technicky udělat, jen vím, že by měl brát karty od indexu 0 dál
+//    if(if datum.contains('dnes') {
+//        dnesniZpravicky.push(kartaSeZpravickou);
+//    }
+//
+//    else() {
+//        console.log('Dnesnich zpravicek na prvni strane je: ');
+//        console.log(dnesniZpravicky.length+1);
+//        break
+//   }
+
+
 
 
 });
