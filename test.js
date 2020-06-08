aa496508b5af1b9599634780b827577e434af737
Vendy Vokalkova <vendula.vokalkova@flowup.cz>
2020-06-08 12:27:43 +0200

    it's broken again

--- test.js
+++ test.js
@@ -12,45 +12,26 @@ describe('Protractor Demo App', function() {
         console.log('I am in!');
         // await browser.sleep(2000);
         const zpravickyLink = await element.all(by.css('.design-navigation__layout-cell .navigation__link[href="/zpravicky/"]')).get(0);
+        console.log('Element link Zprávičky:' + zpravickyLink);
         // const EC = protractor.ExpectedConditions;
         // await browser.wait(EC.invisibilityOf(zpravickyLink),8000);//todo: find why it doesn't work
         await zpravickyLink.click();
         await console.log('Otevře se stránka se Zprávičkami.');
         await browser.sleep(3000);
-        const kartySeZpravickami = await element.all(by.css('.design-article--without-image.design-article.design-tile')).get(0);
-        // const kartaSeZpravickou = kartySeZpravickami.get(0);
-        const datum = kartySeZpravickami.element(by.css('span.design-impressum__item')).getText();
-        await console.log(datum);
-        await expect(await datum).toEqual('Dnes');
-        await browser.sleep(3000);
-
-        for(let i = 0 ; i<5; i++){
-
-
+        const kartySeZpravickami = element.all(by.css('.design-article--without-image.design-article.design-tile'));
+        console.log('Karty se zprávičkami ' + kartySeZpravickami.get(0));
+        console.log('Délka pole karet se zprávičkami: ' + kartySeZpravickami.count());
+            for(let karta = 0; karta <= kartySeZpravickami.count(); karta++) {
+            console.log('Karta: ' + kartysezpravickami.get(karta));
+            console.log(karta);
+            const datum = karta.element(by.css('span.design-impressum__item')).getText();
+            console.log(datum);
+            if (datum === 'Dnes') {
+                let dnesniZpravicky = karta.push();
+            } else {
+                console.log('Na root.cz je dnes ' + dnesniZpravicky.length + ' dnešních zpráviček.');
+                break;
+            }
         }
-
     });
-
-//  const kartaSeZpravickou = driver.getElement.all(by.css('karta se zpravickou');
-//  const spodniCastKarty = kartaSeZpravickou.element(by.css('spodniCastKartyElement');
-//  const datum = spodniCastKart.element(by.css('datum').getText();
-//
-//  const dnesniZpravicky = [];
-//  const zpravicka;
-//
-//  for (i = 0; i < (notNowKnownNumber); i++) {
-//    kartaSeZpravickou.get(index++)//nevím zatím jak to technicky udělat, jen vím, že by měl brát karty od indexu 0 dál
-//    if(if datum.contains('dnes') {
-//        dnesniZpravicky.push(kartaSeZpravickou);
-//    }
-//
-//    else() {
-//        console.log('Dnesnich zpravicek na prvni strane je: ');
-//        console.log(dnesniZpravicky.length+1);
-//        break
-//   }
-
-
-
-
 });
