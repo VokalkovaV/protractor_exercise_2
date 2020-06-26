f7ee1dfa94029725717e150ed8308123dd2b0c22
Vendy Vokalkova <vendula.vokalkova@flowup.cz>
2020-06-26 11:34:43 +0200

    part of page objects implementation

--- po.js
+++ po.js
@@ -1,24 +1,35 @@
-// class Page {
-//     constructor(browser) {
-//         this.browser = browser;
-//     }
-//
-//     getElement(element) {
-//         return this.browser.element(element);
-//     }
-//
-//     getElements(elements) {
-//         return this.browser.element.all(elements);
-//     }
-// }
-//
-// class RootPage extends Page {
-//     constructor(browser) {
-//         super(browser);
-//         this.zpravickyLink = by.css('.design-navigation__layout-cell .navigation__link[href="/zpravicky/"]').get(0);
-//         this.kartySeZpravickami = by.css('.design-article--without-image.design-article.design-tile');
-//         this.datum = by.css('span.design-impressum__item');
-//     }
-// }
-//
-// module.exports.RootPage = RootPage;
+const {By} = require('selenium-webdriver');
+
+class Page {
+    constructor(browser) {
+        this.browser = browser;
+    }
+
+    getZpravickyLink() {
+        return this.zpravickyLink.get(0);
+    }
+
+    getKartySeZpravickami() {
+        return this.kartySeZpravickami;
+    }
+
+    getNumberOfKaretSeZpravickami() {
+        return this.getKartySeZpravickami().count();
+    }
+
+    getDatum(){
+        return this.datum;
+    }
+
+}
+
+class RootPage extends Page {
+    constructor(browser) {
+        super(browser);
+        this.zpravickyLink = element.all(by.css('.design-navigation__layout-cell .navigation__link[href="/zpravicky/"]'));
+        this.kartySeZpravickami = element.all(by.css('.design-article--without-image.design-article.design-tile'));
+        this.datum = element.all(by.css('span.design-impressum__item'));
+    }
+}
+
+module.exports.RootPage = RootPage;
--- test.js
+++ test.js
@@ -1,28 +1,33 @@
 require('geckodriver');
 const webdriver = require('selenium-webdriver');
+const page = require('./po.js');
 
 
 describe('Protractor Demo App', function() {
+    let rootPage;
+
     beforeEach(async function() {
+        rootPage = new page.RootPage();
         await browser.waitForAngularEnabled(false);
         await browser.get('https://www.root.cz');
     });
 
     it('found todays "zprávičky" on the root.com and count them. ', async function () {
         console.log('I am in!');
-        // await browser.sleep(2000);
-        const zpravickyLink = await element.all(by.css('.design-navigation__layout-cell .navigation__link[href="/zpravicky/"]')).get(0);
+
+        await browser.sleep(2000);
+        const zpravickyLink = await rootPage.getZpravickyLink();
         console.log('Element link Zprávičky:' + zpravickyLink);
         // const EC = protractor.ExpectedConditions;
         // await browser.wait(EC.invisibilityOf(zpravickyLink),8000);//todo: find why it doesn't work
         await zpravickyLink.click();
         await console.log('Card with "Zprávičky" page opens.');
         await browser.sleep(1000);
-        let kartySeZpravickami = element.all(by.css('.design-article--without-image.design-article.design-tile'));
-        console.log('Zprávičky cards: ' + kartySeZpravickami.get(0));
-        console.log('Length of the array with Zprávičky cards: ' + kartySeZpravickami.count());
+        let kartySeZpravickami = await rootPage.getKartySeZpravickami();
+        // let kartySeZpravickami = element.all(by.css('.design-article--without-image.design-article.design-tile'));
+        console.log('Length of the array with Zprávičky cards: ' + rootPage.getNumberOfKaretSeZpravickami());
         let dnesniZpravicky = [];
-        let count = await kartySeZpravickami.count();
+        let count = await rootPage.getNumberOfKaretSeZpravickami();
 
         for(let karta = 0; karta <= count; karta++) {
             console.log('Card: ' + kartySeZpravickami.get(karta));
